e30e5f1594542ec3e62bd3355d7123e679fb4483
Taufik Nurrohman <t.nurrohman77@gmail.com>
2015-07-19 23:56:28 +0700

    For Mecha 1.1.3+

--- spoiler/__launch.php
+++ spoiler/__launch.php
@@ -6,14 +6,14 @@
  * --------------
  */
 
-Route::accept($config->manager->slug . '/plugin/' . basename(__DIR__) . '/update', function() use($config, $speak) {
+Route::accept($config->manager->slug . '/plugin/' . File::B(__DIR__) . '/update', function() use($config, $speak) {
     if($request = Request::post()) {
         Guardian::checkToken($request['token']);
         unset($request['token']); // Remove token from request array
-        $css = File::open(PLUGIN . DS . basename(__DIR__) . DS . 'shell' . DS . 'pigment' . DS . $request['skin'] . '.css')->read();
-        File::write(Converter::detractShell($css . "\n\n" . trim($request['css'])))->saveTo(PLUGIN . DS . basename(__DIR__) . DS . 'shell' . DS . 'spoiler.css');
-        File::serialize($request)->saveTo(PLUGIN . DS . basename(__DIR__) . DS . 'states' . DS . 'config.txt', 0600);
+        $css = File::open(PLUGIN . DS . File::B(__DIR__) . DS . 'shell' . DS . 'pigment' . DS . $request['skin'] . '.css')->read();
+        File::write(Converter::detractShell($css . "\n\n" . trim($request['css'])))->saveTo(PLUGIN . DS . File::B(__DIR__) . DS . 'shell' . DS . 'spoiler.css');
+        File::serialize($request)->saveTo(PLUGIN . DS . File::B(__DIR__) . DS . 'states' . DS . 'config.txt', 0600);
         Notify::success(Config::speak('notify_success_updated', $speak->plugin));
-        Guardian::kick(dirname($config->url_current));
+        Guardian::kick(File::D($config->url_current));
     }
 });
--- spoiler/launch.php
+++ spoiler/launch.php
@@ -1,32 +1,25 @@
 <?php
 
-$spoiler_config = File::open(PLUGIN . DS . basename(__DIR__) . DS . 'states' . DS . 'config.txt')->unserialize();
+$spoiler_config = File::open(PLUGIN . DS . File::B(__DIR__) . DS . 'states' . DS . 'config.txt')->unserialize();
 
 Config::set('spoiler_id', 1);
 
 Filter::add('shortcode', function($content) use($spoiler_config) {
-
     // No shortcode pattern found
-    if(strpos($content, '{{spoiler') === false) return $content;
-
+    if( ! Text::check($content)->has('{{spoiler')) return $content;
     $config = Config::get();
     $counter = 0;
-
     $content = preg_replace_callback('#(?<!`)\{\{spoiler(\.([a-zA-Z0-9\-]+))?(\}\}| +.*?\}\})(?!`)([\s\S]*?)(?<!`)\{\{\/spoiler(\.\2)?\}\}(?!`)#', function($matches) use($config, $spoiler_config, &$counter) {
-
         $counter++;
-
         // Fix all shortcode names with descendant definition. The opening and closing tag must be the same.
         // `{{spoiler}} ... {{/spoiler}}` and `{{spoiler.default}} ... {{/spoiler.default}}` are OK
         // but not for `{{spoiler.default}} ... {{/spoiler}}`. It will never be treated as a valid shortcode
-        // pattern just because the end closing of the shortcode is not `{{/spoiler.default}}`
+        // pattern just because the end closing of the shortcode pattern is not `{{/spoiler.default}}`
         if( ! empty($matches[2])) {
             $matches[0] = preg_replace('#\{\{\/spoiler\}\}$#', '{{/spoiler.' . $matches[2] . '}}', $matches[0]);
         }
-
         // Extract shortcode attributes
         $data = Converter::attr($matches[0], array('{{', '}}', ' '), array('"', '"', '='));
-
         // Put all attributes to the HTML output
         $attrs = $data['attributes'];
         $attrs_str  = ' class="spoiler spoiler-' . ( ! empty($matches[2]) ? $matches[2] : 'default') . ' spoiler-state-';
@@ -37,44 +30,32 @@ Filter::add('shortcode', function($content) use($spoiler_config) {
         $attrs_str .= ' data-toggle-text="' . (isset($attrs['toggle-text']) ? $attrs['toggle-text'] : $spoiler_config['toggle_text_open'] . ' | ' . $spoiler_config['toggle_text_close']) . '"';
         $attrs_str .= ' data-toggle-placement="' . (isset($attrs['toggle-placement']) ? $attrs['toggle-placement'] : $spoiler_config['toggle_placement']) . '"';
         $attrs_str .= isset($attrs['connect']) ? ' data-connection="' . $attrs['connect'] . '"' : "";
-
         // Other attributes will be treated as normal HTML attributes
         if(is_array($attrs)) {
-            unset($attrs['expand']);
-            unset($attrs['lock']);
-            unset($attrs['class']);
-            unset($attrs['id']);
-            unset($attrs['toggle-text']);
-            unset($attrs['toggle-placement']);
-            unset($attrs['connect']);
+            unset($attrs['expand'], $attrs['lock'], $attrs['class'], $attrs['id'], $attrs['toggle-text'], $attrs['toggle-placement'], $attrs['connect']);
             foreach($attrs as $key => $value) {
                 $attrs_str .= ' ' . $key . '="' . $value . '"';
             }
         }
-
         return O_BEGIN . '<div' . $attrs_str . '>' . NL . '<div markdown="1" class="spoiler-content">' . $data['content'] . '</div>' . NL . '</div>' . O_END;
-
     }, $content);
-
     Config::set('spoiler_id', $config->spoiler_id + 1);
-
     return $content;
-
-}, 9);
+});
 
 Filter::add('content', function($content) {
     // Remove the `markdown` attribute if it's still there
     return str_replace(' markdown="1" class="spoiler-content"', ' class="spoiler-content"', $content);
-}, 9);
+});
 
 Weapon::add('SHIPMENT_REGION_TOP', function() {
     echo O_BEGIN . str_repeat(TAB, 2) . '<script>document.documentElement.className += \' spoiler-js\';</script>' . O_END;
 });
 
 Weapon::add('shell_after', function() use($spoiler_config) {
-    echo Asset::stylesheet('cabinet/plugins/' . basename(__DIR__) . '/shell/spoiler.css');
+    echo Asset::stylesheet('cabinet/plugins/' . File::B(__DIR__) . '/assets/shell/spoiler.css');
 });
 
 Weapon::add('SHIPMENT_REGION_BOTTOM', function() {
-    echo Asset::javascript('cabinet/plugins/' . basename(__DIR__) . '/sword/spoiler.js');
+    echo Asset::javascript('cabinet/plugins/' . File::B(__DIR__) . '/assets/sword/spoiler.js');
 });
